0446169a65f853184bd9525e3a014f529808f090
Zachary Hansen <zacharyhansen@Zacharys-MacBook-Pro.local>
2018-09-19 20:02:20 -0700

    refactor seed function to create databse and schema automatically

--- database/seed.js
+++ database/seed.js
@@ -1,12 +1,19 @@
 const faker = require('faker');
 const Promise = require('bluebird');
-const {
-  db,
-  Artist,
-  Song,
-  Album,
-  RelatedSongs,
-} = require('./index.js');
+const Sequelize = require('sequelize');
+
+const DATABASE = 'relatedLists';
+
+const USER = 'root';
+
+const PASSWORD = 'blooper2010';
+
+const dbInit = new Sequelize('', USER, PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: false,
+});
+
 
 // Returns random int (1 - max)
 const getRandomInt = max => Math.ceil(Math.random() * Math.floor(max));
@@ -82,22 +89,124 @@ const buildData = (numOfSongs) => {
   };
 };
 
+
 // syncs the data base and drops any pre-existing tables
-const sync = () => db.sync({ force: true });
+const sync = (db) => db.sync({ force: true });
 
 // Create arrays of data, sync the database with the schema, and create table entries with the data
-const seed = () => {
-  const {
-    artists,
-    songs,
-    albums,
-    relatedTracks,
-  } = buildData(100);
-  sync()
-    .then(() => Promise.each(artists, artist => Artist.create(artist)))
-    .then(() => Promise.each(songs, song => Song.create(song)))
-    .then(() => Promise.each(albums, album => Album.create(album)))
-    .then(() => Promise.each(relatedTracks, relatedTrack => RelatedSongs.create(relatedTrack)));
-};
+// const seed = (db) => {
+//   const {
+//     artists,
+//     songs,
+//     albums,
+//     relatedTracks,
+//   } = buildData(100);
+//   sync(db)
+//     .then(() => Promise.each(artists, artist => Artist.create(artist)))
+//     .then(() => Promise.each(songs, song => Song.create(song)))
+//     .then(() => Promise.each(albums, album => Album.create(album)))
+//     .then(() => Promise.each(relatedTracks, relatedTrack => RelatedSongs.create(relatedTrack)));
+// };
 
-seed();
+// Search for Databae and Create it if none exists
+dbInit.query(`CREATE DATABASE IF NOT EXISTS ${DATABASE}`)
+  .then(() => {
+    console.log(`Database ${DATABASE} created`);
+
+    const db = new Sequelize(DATABASE, USER, PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      logging: false,
+    });
+
+    // Model Definition
+    const Artist = db.define('artist', {
+      artistNumFollowers: {
+        type: Sequelize.INTEGER,
+      },
+      artistCity: {
+        type: Sequelize.STRING,
+      },
+      artistCountry: {
+        type: Sequelize.STRING,
+      },
+      artistProfileImage: {
+        type: Sequelize.STRING,
+      },
+      artistVerified: {
+        type: Sequelize.BOOLEAN,
+      },
+      artistName: {
+        type: Sequelize.STRING,
+      },
+    });
+
+    const Song = db.define('song', {
+      songTitle: {
+        type: Sequelize.STRING,
+      },
+      songImage: {
+        type: Sequelize.STRING,
+      },
+      songLength: {
+        type: Sequelize.STRING,
+      },
+      songGenre: {
+        type: Sequelize.STRING,
+      },
+      songPostedDate: {
+        type: Sequelize.STRING,
+      },
+      songNumPlays: {
+        type: Sequelize.INTEGER,
+      },
+      songNumLikes: {
+        type: Sequelize.INTEGER,
+      },
+      songNumReposts: {
+        type: Sequelize.INTEGER,
+      },
+      songNumComments: {
+        type: Sequelize.INTEGER,
+      },
+    });
+
+    const Album = db.define('album', {
+      albumTitle: {
+        type: Sequelize.STRING,
+      },
+      albumType: {
+        type: Sequelize.STRING,
+      },
+      albumImage: {
+        type: Sequelize.STRING,
+      },
+    });
+
+    const RelatedSongs = db.define('relatedSongs', {});
+
+    // Establish Song/Artist relationship - Each Song will have a foreign key referencing its Artist
+    Song.belongsTo(Artist);
+    Artist.hasMany(Song, { as: 'tracks' });
+
+    // Establish Related Tracks relationship where one song has many songs (3 related tracks)
+    Song.belongsToMany(Song, { as: 'relatedTracks', through: RelatedSongs });
+
+    // Establish Album relationships - Each Song belongs to 3 unique Albums,
+    // an Album belongs to an Artist, and an Artist has many Albums
+    Album.belongsTo(Song);
+    Album.belongsTo(Artist);
+    Artist.hasMany(Album);
+
+    const {
+      artists,
+      songs,
+      albums,
+      relatedTracks,
+    } = buildData(100);
+    sync(db)
+      .then(() => Promise.each(artists, artist => Artist.create(artist)))
+      .then(() => Promise.each(songs, song => Song.create(song)))
+      .then(() => Promise.each(albums, album => Album.create(album)))
+      .then(() => Promise.each(relatedTracks, relatedTrack => RelatedSongs.create(relatedTrack)));
+  });
